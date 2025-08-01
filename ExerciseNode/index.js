3ef585caaca1f95c2be53499b4b83c7d27575157
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-08-01 11:27:41 -0500

    C1

--- ExerciseNode/index.js
+++ ExerciseNode/index.js
@@ -1,19 +1,61 @@
-// Con la parte de abajo traigo el paquete que esta en la parte de abajo
-// El paquete es asincrono
-const axios = require('axios');
+// // Con la parte de abajo traigo el paquete que esta en la parte de abajo
+// // El paquete es asincrono
+// const axios = require('axios');
 
-const main = async() => {
-    let response = await axios.get('https://rickandmortyapi.com/api/character');
-    let {data: {results}} = response;
-    let characters = results.map((character) => {
-        return {
-            id: character.id,
-            name: character.name,
-            status: character.status,
-            species: character.species
-        }
-    }).map((personaje) => Object.values(personaje).join(",")).join("\n");
-    console.log(characters);
-};
-// MAIN
-main();
+// const main = async() => {
+//     let response = await axios.get('https://rickandmortyapi.com/api/character');
+//     let {data: {results}} = response;
+//     let characters = results.map((character) => {
+//         return {
+//             id: character.id,
+//             name: character.name,
+//             status: character.status,
+//             species: character.species
+//         }
+//     }).map((personaje) => Object.values(personaje).join(",")).join("\n");
+//     console.log(characters);
+// };
+// // MAIN
+// main(); 
+
+
+
+const characters = {
+    Batman:{
+        age:28,
+        weight:78,
+        power:"Strenght",
+        color: "black"
+    },
+    Hulk:{
+        age:25,
+        weight:18,
+        power:"Strenght",
+        color: "Green"
+    },
+    Thor:{
+        age:34,
+        weight:28,
+        power:"Flying",
+        color: "blue"
+    },
+    Popeye:{
+        age:45,
+        weight:38,
+        power:"Strenght",
+        color: "white"
+    }
+} 
+
+
+const datos = (callback) => {
+    callback({
+        name:"Andres",
+        Edad:25
+    }),
+    3000};
+
+const imprimir = (datos) => console.log(datos);
+    
+datos(imprimir);
+
