becff332b9cfd9ddf1dfd04345cedfa7f30ff5e5
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-08-08 17:50:01 -0500

    C1

--- Callbacks.js
+++ Callbacks.js
@@ -2,20 +2,33 @@
 // that each thing don't execute in orden if not in less process until biggest process
 // now, with callbacks we can recive the functions o datas until we required.
 
-const Getdata = (Cb, CbFail) => {
-    if (true) {
-        setTimeout(() => {
-            Cb({
-            name: "Cristian",
-            Age: 21,
-            Carrer: "Engineer"
-            })
-    }, 2000);
-    } else {
-        CbFail(new Error("We can't get the data"))
-    }
+    // const Getdata = (Cb, CbFail) => {
+    //     if (true) {
+    //         setTimeout(() => {
+    //             Cb({
+    //             name: "Cristian",
+    //             Age: 21,
+    //             Carrer: "Engineer"
+    //             })
+    //     }, 2000);
+    //     } else {
+    //         CbFail(new Error("We can't get the data"))
+    //     }
+    // }
+
+    // const Print = (data) => console.log(data);
+
+    // Getdata(Print);
+        
+
+
+
+
+
+const Speak = (name,cb) => {
+        cb("How it's going?");
 }
 
-const Print = (data) => console.log(data);
-
-Getdata(Print);
+Speak("Andres",(cbb) => {
+        console.log(cbb,"Good  and u?")
+})
