1de7a743538f00d802fed50faaf90dd86fdae0ac
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-08-13 19:33:15 -0500

    C5

--- ExerciseNode/test.js
+++ ExerciseNode/test.js
@@ -1,15 +1,32 @@
-let paquete = {
-  meta: {
-    version: "1.0",
-    autor: "Cristian"
-  },
-  contenido: {
-    frutas: ["🍎", "🍌", "🍇"],
-    verduras: ["🥦", "🥕"]
-  },
-  estado: "listo"
-}; 
+// let paquete = {
+//   meta: {
+//     version: "1.0",
+//     autor: "Cristian"
+//   },
+//   contenido: {
+//     frutas: ["🍎", "🍌", "🍇"],
+//     verduras: ["🥦", "🥕"]
+//   },
+//   estado: "listo"
+// }; 
 
-let {contenido:{verduras}} = paquete 
+// let {contenido:{verduras}} = paquete 
 
-console.log("Aqui las verduras --> ", verduras);
+// console.log("Aqui las verduras --> ", verduras);
+
+const axios = require('axios');
+
+const main = async() => {
+    let getting = await axios.get('https://rickandmortyapi.com/api/character');
+    let {data:{results}} = getting;
+
+    const characters = results.map((character) => {
+            return {
+                name: character.name,
+                species: character.species
+            }
+     })
+    console.log(characters);
+}
+
+main()
