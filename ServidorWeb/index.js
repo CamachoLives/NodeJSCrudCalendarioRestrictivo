a44f1879ad0d2c4c5c914a5d9a254160ab143e38
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-08-24 13:44:03 -0500

    Iniciacion de clase de Servidor

---
+++ ServidorWeb/index.js
@@ -0,0 +1,17 @@
+// the sheet that will contain all above the Server
+
+const express = require("express");
+const app = express();
+const PORT = 4000;
+const data = require("./MOCK_DATA.json");
+
+app.get("/", (Req, Res) => {
+  Res.json({
+    message: "List Users",
+    body: data,
+  });
+});
+
+app.listen(4000, () => {
+  console.log(`Server listening --> http://localhost:${PORT}`);
+});
