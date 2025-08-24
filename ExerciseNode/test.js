7cc83c5cc7c8ac8eb72cbda76ff97cd3be01a8d3
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-08-23 23:43:16 -0500

    C1

--- ExerciseNode/test.js
+++ ExerciseNode/test.js
@@ -8,35 +8,43 @@
 //     verduras: ["🥦", "🥕"]
 //   },
 //   estado: "listo"
-// }; 
+// };
 
-// let {contenido:{verduras}} = paquete 
+// let {contenido:{verduras}} = paquete
 
 // console.log("Aqui las verduras --> ", verduras);
 
-const axios = require('axios');
-const fs = require('fs').promises;
-const path = require('path');
+const axios = require("axios");
+const fs = require("fs").promises;
+const path = require("path");
 
-const main = async() => {
-    let getting = await axios.get('https://rickandmortyapi.com/api/character');
-    let {data:{results}} = getting;
+const main = async () => {
+  let getting = await axios.get("https://rickandmortyapi.com/api/character");
+  let {
+    data: { results },
+  } = getting;
 
-    const characters = results.map((character) => {
-            return {
-                id: character.id,
-                name: character.name,
-                species: character.species
-            }
-     }).map((Valores) => Object.values(Valores).join(",")).join("\n")
+  const characters = results
+    .map((character) => {
+      return {
+        id: character.id,
+        name: character.name,
+        species: character.species,
+      };
+    })
+    .map((Valores) => Object.values(Valores).join(","))
+    .join("\n");
 
-     const cabecera = "Id,Name,Specie"
-    await fs.writeFile(path.join(__dirname,"Datos1.csv"), cabecera + "\n" + characters);
+  const cabecera = "Id,Name,Specie";
+  await fs.writeFile(
+    path.join(__dirname, "Datos1.csv"),
+    cabecera + "\n" + characters
+  );
 
-    // console.log(path.join(__dirname,"Datos.csv"));
-    // const cabecera = "Id,Name,Specie"
-    // console.log(cabecera + "\n" + characters);
-    // CC
-}
+  // console.log(path.join(__dirname,"Datos.csv"));
+  // const cabecera = "Id,Name,Specie"
+  // console.log(cabecera + "\n" + characters);
+  //
+};
 
-main()
+main();
