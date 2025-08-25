3c93ce852b6ee59a024dc99b14db3fcebe4b58ce
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-08-24 23:17:56 -0500

    Already create the index

--- ServidorWeb/index.js
+++ ServidorWeb/index.js
@@ -2,16 +2,31 @@
 //Variables
 const express = require("express");
 const app = express();
+const Service = require("./src/service");
 const PORT = 4000;
-const data = require("./MOCK_DATA.json");
+// const data = require("./MOCK_DATA.json");
 
+// this middleware will allow us to read the body of the request
+app.use(express.json());
+
+// This endpoint will allow us to read the body of the request
 app.get("/", (Req, Res) => {
   Res.json({
     message: "List Users",
-    body: data,
+    body: Service.getusers(),
   });
 });
 
+// this endpoint will allow us to create a new user
+app.post("/", (Req, Res) => {
+  const newUser = Req.body;
+  Res.json({
+    message: "User Created  Successfully",
+    body: Service.CreateUser(newUser),
+  });
+});
+
+// this will start the server
 app.listen(4000, () => {
   console.log(`Server listening --> http://localhost:${PORT}`);
 });
---
+++ ServidorWeb/src/service.js
@@ -0,0 +1,17 @@
+// this file will manage the services of the application
+const data = require("./MOCK_DATA.json");
+
+module.exports = {
+  // this function will return all users
+  getusers: () => data,
+
+  // this function will create a new user
+  CreateUser: (DataUser) => {
+    let newUser = {
+      id: data.length + 1,
+      ...DataUser,
+    };
+    data.push(newUser);
+    return newUser;
+  },
+};
