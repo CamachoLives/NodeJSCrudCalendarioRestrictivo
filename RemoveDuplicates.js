28f9095ba019b4b80734aa0cf9ced0cbdb2056c8
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-09-13 13:06:26 -0500

    New Challenge about how remove duplicates

---
+++ RemoveDuplicates.js
@@ -0,0 +1,15 @@
+let nums = [1, 1, 2, 2, 3, 4, 5, 5];
+
+function removeDuplicates(nums) {
+  let z = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] != nums[i - 1]) {
+      nums[z] = nums[i];
+      z++;
+    }
+    
+  }
+  return z;
+}
+
+console.log(removeDuplicates(nums));
