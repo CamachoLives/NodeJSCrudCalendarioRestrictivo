fef11d4b502540ad8784ccb00aa6deed96f6bc82
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-09-16 14:08:29 -0500

    Fixed the problem

--- ReverseInteger.js
+++ ReverseInteger.js
@@ -1,14 +1,25 @@
 int = 3456;
 var reverse = function (int) {
-  let r = [];
+  let r = null;
+  let contador = 0;
+  let temporal = 0;
 
   while (int > 0) {
     let digito = int % 10;
-    r.push(digito);
-    int = Math.floor(int / 10);
-  }
 
-  return r;
+    if (contador == 0) {
+      r = digito;
+      console.log(r);
+      contador++;
+    } else {
+      r = "" + temporal + digito;
+    }
+    int = Math.floor(int / 10);
+    temporal = r;
+  }
+  int = r
+
+  return parseInt(int);
 };
 
 console.log(reverse(int));
