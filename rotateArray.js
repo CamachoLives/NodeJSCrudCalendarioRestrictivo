d78d36bcd7df1316113fd3314878d4324bdb9c5c
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-09-16 18:09:47 -0500

    i have finished it

--- rotateArray.js
+++ rotateArray.js
@@ -1,15 +1,29 @@
-const nums = [1, 2, 3, 4, 5, 6, 7],
-  k = 3;
+const nums = [1, 2],
+  k = 0;
 var rotate = function (nums, k) {
+  k = k % nums.length;
   const parte = nums.slice(-k);
   console.log(parte);
 
   const parte1 = nums.slice(0, nums.length - k);
   console.log(parte1);
 
-  const union = parte.concat(parte1);
-  console.log(union);
-  return union;
+  if (nums.length > 1) {
+    if (k == 0) {
+      return nums;
+    } else {
+      const union = parte.concat(parte1);
+      console.log(union);
+      for (let y = 0; y < union.length; y++) {
+        nums[y] = union[y];
+        console.log(nums[y]);
+      }
+
+      return nums;
+    }
+  } else {
+    return nums;
+  }
 };
 
 console.log(rotate(nums, k));
