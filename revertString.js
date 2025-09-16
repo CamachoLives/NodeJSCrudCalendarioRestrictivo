2744d6d51c9156dc01dd64e1aa39ab41272bb18b
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-09-15 22:46:07 -0500

    Solution

--- revertString.js
+++ revertString.js
@@ -1,5 +1,14 @@
 s = ["h", "e", "l", "l", "o"];
 var reverseString = function (s) {
-
-    
+  let z = 0;
+  let k = s.length - 1;
+  while (z < k) {
+    let temporal = s[z];
+    s[z] = s[k];
+    s[k] = temporal;
+  }
+  console.log(s);
+  return s;
 };
+
+console.log(reverseString(s));
