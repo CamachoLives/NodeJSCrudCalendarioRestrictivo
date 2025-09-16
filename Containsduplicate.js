87ce8df5e254381c6cd9d5ebe36f9ddc342520c0
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-09-16 18:18:03 -0500

    later i'll continue

---
+++ Containsduplicate.js
@@ -0,0 +1,13 @@
+let nums = [1, 2, 3, 1];
+var containsDuplicate = function (nums) {
+  IsTrue = false;
+  Temp = null
+  for (x = 0; x > nums.length; x++) {
+    if (Temp == nums[x]){
+        
+    }else{
+
+    }
+
+  }
+};
