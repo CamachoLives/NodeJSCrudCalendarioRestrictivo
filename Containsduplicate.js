03714a4840efbe9fbc99a1db04a2ff63186e2789
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-09-17 01:25:08 -0500

    Solved at my style

--- Containsduplicate.js
+++ Containsduplicate.js
@@ -1,13 +1,15 @@
-let nums = [1, 2, 3, 1];
+let nums = [1, 3, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 var containsDuplicate = function (nums) {
-  IsTrue = false;
-  Temp = null
-  for (x = 0; x > nums.length; x++) {
-    if (Temp == nums[x]){
-        
-    }else{
-
+  arreglo = [];
+  valor = null;
+  for (x = 0; x < nums.length; x++) {
+    arreglo.push(nums[x]);
+    valor = nums[x + 1];
+    if (arreglo.includes(valor)) {
+      return true;
     }
-
   }
+  return false;
 };
+
+console.log(containsDuplicate(nums));
