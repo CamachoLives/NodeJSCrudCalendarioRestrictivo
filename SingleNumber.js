5238cf8a253d57eca32666913dba797b41d45fcb
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-09-19 14:46:58 -0500

    Complemented the exercise

--- SingleNumber.js
+++ SingleNumber.js
@@ -1,4 +1,9 @@
-let nums = [7,3,3,4,4];
-var singleNumber = function(nums){
-    
-}
+let nums = [3, 8, 3, 3, 4, 4];
+var singleNumber = function (nums) {
+  const valoresUnicos = nums.filter(
+    (valor) => nums.indexOf(valor) === nums.lastIndexOf(valor)
+  );
+  return valoresUnicos;
+};
+
+console.log(singleNumber(nums));
